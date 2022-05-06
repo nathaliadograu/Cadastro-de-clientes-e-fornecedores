b6ebf579dfd4a664f7b7f7a6c3afe255a2128345
nathaliadograu <nathaliasouza10222@gmail.com>
2022-05-06 10:23:14 -0300

    Finalizar

--- BD/read.php
+++ BD/read.php
@@ -1,1 +1,18 @@
-
+<?php
+include_once 'conexao.php';
+$querySelect=$link->query("select * from clientes");
+while($registros=$querySelect->fetch_assoc()):
+$id=$registros['id'];
+$nome=$registros['nome'];
+$email=$registros['email'];
+$telefone=$registros['telefone'];
+echo "<tr>";
+echo"
+<td>$nome</td>
+<td>$email</td>
+<td>$telefone</td>
+<td><a href='editar.php?id=$id'><i class='material-ícons'>edit</i></a></td>
+<td><a href='BD/delete.php?id=$id'><i class='material-ícons'>delete</i></a></td>
+";
+echo"</tr>";
+endwhile;
